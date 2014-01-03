2af7a53699605e049e43f5829b5f8f950bbed2a0
KirsiYin <khanh.nv@outlook.com>
2014-01-03 08:48:36 +0700

    1

---
+++ Assets/Score.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+	[HideInInspector]
+	public int playerLeft = 0;
+	[HideInInspector]
+	public int playerRight = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print("asd");
+		GetComponent<TextMesh>().text = playerLeft + " : " + playerRight;
+	}
+}
